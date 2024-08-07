4dc950552d55305bdde09f808c95505f040b1f2e
Pete Slater <28756214+peteslater-ee@users.noreply.github.com>
2024-08-07 08:16:16 +0100

    API-7767 - Adding run scripts

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,18 +7,26 @@ object AppDependencies {
 
   private val bootstrapVersion = "9.2.0"
   private val hmrcMongoVersion = "2.2.0"
+  private val apiDomainVersion = "0.16.0"
+  private val appDomainVersion = "0.55.0"
+  private val tpdDomainVersion = "0.5.0"
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30" % "10.6.0",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"         % hmrcMongoVersion
+    "uk.gov.hmrc"             %% "bootstrap-frontend-play-30"             % bootstrapVersion,
+    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30"             % "10.6.0",
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"                     % hmrcMongoVersion,
+    "uk.gov.hmrc"             %% "api-platform-api-domain"                % apiDomainVersion,
+    "uk.gov.hmrc"             %% "api-platform-application-domain"        % appDomainVersion,
+    "uk.gov.hmrc"             %% "api-platform-tpd-domain"                % tpdDomainVersion
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % bootstrapVersion            % Test,
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30"    % hmrcMongoVersion            % Test,
-    "org.jsoup"               %  "jsoup"                      % "1.13.1"            % Test,
-  )
+    "uk.gov.hmrc"             %% "bootstrap-test-play-30"                 % bootstrapVersion,
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30"                % hmrcMongoVersion,
+    "org.jsoup"               %  "jsoup"                                  % "1.13.1",
+    "uk.gov.hmrc"             %% "api-platform-test-api-domain"           % apiDomainVersion,
+    "uk.gov.hmrc"             %% "api-platform-test-tpd-domain"           % tpdDomainVersion
+  ).map(_ % "test")
 
   val it = Seq.empty
 }
