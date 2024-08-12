7ed8567c60549462ba3fbc254dc0ec3f9783349f
Pete Slater <28756214+peteslater-ee@users.noreply.github.com>
2024-08-12 10:41:11 +0100

    API-7767 - Added another new test

---
+++ test/uk/gov/hmrc/devhubsupportfrontend/controllers/HelpWithSigningInControllerSpec.scala
@@ -0,0 +1,163 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.devhubsupportfrontend.controllers
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+import play.api.mvc.Result
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import uk.gov.hmrc.apiplatform.modules.tpd.test.builders.UserBuilder
+import uk.gov.hmrc.apiplatform.modules.tpd.test.utils.LocalUserIdTracker
+
+import uk.gov.hmrc.devhubsupportfrontend.config.ErrorHandler
+import uk.gov.hmrc.devhubsupportfrontend.controllers.BaseControllerSpec
+import uk.gov.hmrc.devhubsupportfrontend.domain.models.{SupportFlow, SupportSessionId}
+import uk.gov.hmrc.devhubsupportfrontend.mocks.connectors.ThirdPartyDeveloperConnectorMockModule
+import uk.gov.hmrc.devhubsupportfrontend.mocks.service.SupportServiceMockModule
+import uk.gov.hmrc.devhubsupportfrontend.utils.WithCSRFAddToken
+import uk.gov.hmrc.devhubsupportfrontend.utils.WithLoggedInSession._
+import uk.gov.hmrc.devhubsupportfrontend.views.html.support.{HelpWithSigningInView, RemoveAccessCodesView}
+
+class HelpWithSigningInControllerSpec extends BaseControllerSpec with WithCSRFAddToken {
+
+  trait Setup extends SupportServiceMockModule with ThirdPartyDeveloperConnectorMockModule with UserBuilder with LocalUserIdTracker {
+    val helpWithUsingAnApiView = app.injector.instanceOf[HelpWithSigningInView]
+    val removeAccessCodesView  = app.injector.instanceOf[RemoveAccessCodesView]
+
+    lazy val request = FakeRequest()
+      .withSupport(underTest, cookieSigner)(supportSessionId)
+      .withUser(underTest, cookieSigner)(sessionId)
+
+    val underTest        = new HelpWithSigningInController(
+      mcc,
+      cookieSigner,
+      mock[ErrorHandler],
+      ThirdPartyDeveloperConnectorMock.aMock,
+      SupportServiceMock.aMock,
+      helpWithUsingAnApiView,
+      removeAccessCodesView
+    )
+    val supportSessionId = SupportSessionId.random
+    val basicFlow        = SupportFlow(supportSessionId, "?")
+    val appropriateFlow  = basicFlow.copy(entrySelection = SupportData.SigningIn.id)
+
+    ThirdPartyDeveloperConnectorMock.FetchSession.succeeds()
+
+    def shouldBeRedirectedToPreviousPage(result: Future[Result]) = {
+      status(result) shouldBe SEE_OTHER
+      redirectLocation(result).value shouldBe "/devhub-support/new-support"
+    }
+
+    def shouldBeRedirectedToNextPage(result: Future[Result]) = {
+      status(result) shouldBe SEE_OTHER
+      redirectLocation(result).value shouldBe "/devhub-support/new-support/details"
+    }
+
+    def shouldBeRedirectedToRemoveAccessCodesPage(result: Future[Result]) = {
+      status(result) shouldBe SEE_OTHER
+      redirectLocation(result).value shouldBe "/devhub-support/new-support/signing-in/remove-access-codes"
+    }
+  }
+
+  "HelpWithSigningInController" when {
+    "invoking page()" should {
+      "render the HelpWithSigningInView" in new Setup() {
+        SupportServiceMock.GetSupportFlow.succeeds(appropriateFlow)
+
+        val result = addToken(underTest.page())(request)
+
+        status(result) shouldBe OK
+      }
+
+      "render the previous page when flow is wrong" in new Setup {
+        SupportServiceMock.GetSupportFlow.succeeds(basicFlow.copy(entrySelection = "Something else"))
+
+        val result = addToken(underTest.page())(request)
+
+        shouldBeRedirectedToPreviousPage(result)
+      }
+
+      "render the previous page when there is no flow" in new Setup {
+        SupportServiceMock.GetSupportFlow.succeeds(basicFlow)
+
+        val result = addToken(underTest.page())(request)
+
+        shouldBeRedirectedToPreviousPage(result)
+      }
+    }
+
+    "invoking removeAccessCodesPage()" should {
+      "render the RemoveAccessCodesView" in new Setup() {
+        SupportServiceMock.GetSupportFlow.succeeds(appropriateFlow)
+
+        val result = addToken(underTest.removeAccessCodesPage())(request)
+
+        status(result) shouldBe OK
+      }
+    }
+
+    "invoke submit" should {
+      "submit new valid request from form for 'Forgotten Password' choice" in new Setup {
+        val formRequest = request.withFormUrlEncodedBody(
+          "choice" -> SupportData.ForgottenPassword.id
+        )
+        SupportServiceMock.GetSupportFlow.succeeds(appropriateFlow)
+        SupportServiceMock.UpdateWithDelta.succeeds()
+
+        val result = addToken(underTest.submit())(formRequest)
+
+        shouldBeRedirectedToNextPage(result)
+      }
+
+      "submit new valid request from form for 'Access Codes' choice" in new Setup {
+        val formRequest = request.withFormUrlEncodedBody(
+          "choice" -> SupportData.AccessCodes.id
+        )
+        SupportServiceMock.GetSupportFlow.succeeds(appropriateFlow)
+        SupportServiceMock.UpdateWithDelta.succeeds()
+
+        val result = addToken(underTest.submit())(formRequest)
+
+        shouldBeRedirectedToRemoveAccessCodesPage(result)
+      }
+
+      "submit invalid request returns BAD_REQUEST" in new Setup {
+        val formRequest = request.withFormUrlEncodedBody(
+          "bobbins" -> SupportData.AccessCodes.id
+        )
+        SupportServiceMock.GetSupportFlow.succeeds(appropriateFlow)
+
+        val result = addToken(underTest.submit())(formRequest)
+
+        status(result) shouldBe BAD_REQUEST
+      }
+
+      "submit valid request but no session" in new Setup {
+        val formRequest = request.withFormUrlEncodedBody(
+          "choice" -> SupportData.AccessCodes.id
+        )
+        SupportServiceMock.GetSupportFlow.succeeds()
+
+        val result = addToken(underTest.submit())(formRequest)
+
+        shouldBeRedirectedToPreviousPage(result)
+      }
+    }
+  }
+}
