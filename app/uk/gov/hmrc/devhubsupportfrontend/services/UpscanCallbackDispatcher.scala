c3eda59572ddb09ffadb08ade3b0fcf39d2bd94f
kskourtis <kskourtis1@users.noreply.github.com>
2025-07-17 20:56:48 +0100

    APIS-7680: Spike - Inbound attachments via the support form / portal (Upscan)

---
+++ app/uk/gov/hmrc/devhubsupportfrontend/services/UpscanCallbackDispatcher.scala
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.devhubsupportfrontend.services
+
+import uk.gov.hmrc.devhubsupportfrontend.controllers.{CallbackBody, FailedCallbackBody, ReadyCallbackBody}
+import uk.gov.hmrc.devhubsupportfrontend.domain.models.upscan.UploadStatus
+import uk.gov.hmrc.http.HeaderCarrier
+
+import javax.inject.Inject
+import scala.concurrent.Future
+import uk.gov.hmrc.http.HeaderCarrier
+
+import javax.inject.Inject
+import scala.concurrent.Future
+
+class UpscanCallbackDispatcher @Inject() {
+
+  def handleCallback(callback: CallbackBody)
+                    (implicit hc: HeaderCarrier): Future[Unit] = {
+    val uploadStatus =
+      callback match {
+        case s: ReadyCallbackBody =>
+          UploadStatus.UploadedSuccessfully(
+            name        = s.uploadDetails.fileName,
+            mimeType    = s.uploadDetails.fileMimeType,
+            downloadUrl = s.downloadUrl,
+            size        = Some(s.uploadDetails.size)
+          )
+        case _: FailedCallbackBody =>
+          UploadStatus.Failed
+      }
+
+    println(s"Upscan upload result: $uploadStatus")
+
+    Future.successful()
+  }
+}
---
+++ app/uk/gov/hmrc/devhubsupportfrontend/utils/HttpUrlFormat.scala
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.devhubsupportfrontend.utils
+
+import play.api.libs.json._
+
+import java.net.URL
+import scala.util.Try
+
+object HttpUrlFormat {
+
+  implicit val format: Format[URL] = new Format[URL] {
+    override def reads(json: JsValue): JsResult[URL] = json match {
+      case JsString(s) =>
+        parseUrl(s).map(JsSuccess(_)).getOrElse(JsError(Seq(JsPath() -> Seq(JsonValidationError("error.expected.url")))))
+      case _ => JsError(Seq(JsPath() -> Seq(JsonValidationError("error.expected.url"))))
+    }
+
+    private def parseUrl(s: String): Option[URL] = Try(new URL(s)).toOption
+
+    override def writes(o: URL): JsValue = JsString(o.toString)
+  }}
