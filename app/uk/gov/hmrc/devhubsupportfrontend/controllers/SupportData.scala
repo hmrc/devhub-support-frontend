3c10c35a590dedf8956c02604550b705b6a8812e
petekirby-ee <pete.kirby@digital.hmrc.gov.uk>
2025-07-25 11:49:29 +0100

    APIS-7721 Completing the terms of use agreement

--- app/uk/gov/hmrc/devhubsupportfrontend/controllers/SupportData.scala
+++ app/uk/gov/hmrc/devhubsupportfrontend/controllers/SupportData.scala
@@ -90,12 +90,12 @@ object SupportData {
 
   case object GivingTeamMemberAccess extends ApiSecondaryChoice {
     val id   = "giving-team-member-access"
-    val text = "Giving a team member access to a software application"
+    val text = "Giving a team member access to an application"
   }
 
   case object GeneralApplicationDetails extends ApiSecondaryChoice {
     val id   = "general-application-details"
-    val text = "None of these"
+    val text = "General application details"
   }
 
   case object ChooseBusinessRates {
