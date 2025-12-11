ae60859950a90b44c42c04e0ca4245a994ef9163
kskourtis <kskourtis1@users.noreply.github.com>
2025-12-11 10:29:44 +0000

    APIS-7866: Frontend Create Support Ticket Flow - update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,19 +3,19 @@ import sbt._
 object AppDependencies {
 
   private val bootstrapVersion = "9.19.0"
-  private val hmrcMongoVersion = "2.10.0"
+  private val hmrcMongoVersion = "2.11.0"
   private val apiDomainVersion = "0.20.0"
   private val tpdDomainVersion = "0.14.0"
 
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-frontend-play-30"             % bootstrapVersion,
-    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30"             % "12.21.0",
+    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30"             % "12.22.0",
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"                     % hmrcMongoVersion,
     "commons-validator"       %  "commons-validator"                      % "1.9.0",
     "uk.gov.hmrc"             %% "http-metrics"                           % "2.9.0",
     "uk.gov.hmrc"             %% "api-platform-api-domain"                % apiDomainVersion,
     "uk.gov.hmrc"             %% "api-platform-tpd-domain"                % tpdDomainVersion,
-    "uk.gov.hmrc"             %% "play-conditional-form-mapping-play-30"  % "3.3.0"
+    "uk.gov.hmrc"             %% "play-conditional-form-mapping-play-30"  % "3.4.0"
   )
 
   val test = Seq(
