35de8506bd92a032aa724473a7b4fb6a62617b3a
Neil Frow <675806+worthydolt@users.noreply.github.com>
2026-03-26 10:39:57 +0000

    API-9026: bump dependencies and address warnings (#111)
    * API-9026: bump dependencies and address warnings
    
    * API-9026: bump more dependencies
    
    * API-9026 - Bump to cross compiled lib
    
    ---------
    
    Co-authored-by: Andy Spaven <16537059+AndySpaven@users.noreply.github.com>

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,27 +2,27 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.19.0"
-  private val hmrcMongoVersion = "2.11.0"
-  private val apiDomainVersion = "0.20.0"
-  private val tpdDomainVersion = "0.14.0"
+  private val bootstrapVersion = "10.7.0"
+  private val hmrcMongoVersion = "2.12.0"
+  private val apiDomainVersion = "1.0.0"
+  private val tpdDomainVersion = "0.15.0"
 
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-frontend-play-30"             % bootstrapVersion,
-    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30"             % "12.22.0",
+    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30"             % "12.32.0",
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"                     % hmrcMongoVersion,
     "commons-validator"       %  "commons-validator"                      % "1.9.0",
     "uk.gov.hmrc"             %% "http-metrics"                           % "2.9.0",
     "uk.gov.hmrc"             %% "api-platform-api-domain"                % apiDomainVersion,
     "uk.gov.hmrc"             %% "api-platform-tpd-domain"                % tpdDomainVersion,
-    "uk.gov.hmrc"             %% "play-conditional-form-mapping-play-30"  % "3.4.0"
+    "uk.gov.hmrc"             %% "play-conditional-form-mapping-play-30"  % "3.5.0"
   )
 
   val test = Seq(
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"                 % bootstrapVersion,
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30"                % hmrcMongoVersion,
-    "org.jsoup"               %  "jsoup"                                  % "1.18.1",
-    "uk.gov.hmrc"             %% "api-platform-test-api-domain"           % apiDomainVersion,
+    "org.jsoup"               %  "jsoup"                                  % "1.22.1",
+    "uk.gov.hmrc"             %% "api-platform-api-domain-fixtures"       % apiDomainVersion,
     "uk.gov.hmrc"             %% "api-platform-test-tpd-domain"           % tpdDomainVersion
   ).map(_ % "test")
 
